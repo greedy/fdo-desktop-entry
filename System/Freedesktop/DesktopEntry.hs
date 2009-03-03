671ea88a96e506eb44588b1ef2c38c4a05c945de
Geoff Reedy <gereedy@sandia.gov>
2009-03-03 13:36:04 -0700

    Implement parsing and fix module exports.
    Add parsing functions fromContents and fromFile.
    
    Add exports for StringVal(..), unwrap, get
    
    Fix module header
    
    Add parsec dependency to cabal file
    
    Relax version requirements in cabal file

--- System/Freedesktop/DesktopEntry.hs
+++ System/Freedesktop/DesktopEntry.hs
@@ -1,6 +1,6 @@
 {- |
-Module      :   System/Freedesktop/Directories.hs
-Description :   Implements freedesktop.org basedir specification
+Module      :   System/Freedesktop/DesktopEntry.hs
+Description :   Implements freedesktop.org desktop-entry specification
 Copyright   :   (c) Geoffrey Reedy
 License     :   BSD3
 
@@ -17,16 +17,21 @@ module System.Freedesktop.DesktopEntry (
   Key (..),
   Failure,
   Value,
+  StringVal(..),
+  unwrap,
   Locale(..),
-  DesktopEntry
+  DesktopEntry,
+  get,
+  fromContents,
+  fromFile
 ) where
 
 import Data.List
 import Data.Maybe
 import Data.String
 import Control.Monad.Error
-import Control.Applicative
 import qualified Data.Map as M
+import Text.ParserCombinators.Parsec
 
 data Key = Key String (Maybe Locale)
   deriving (Show, Eq, Ord)
@@ -34,9 +39,11 @@ data Key = Key String (Maybe Locale)
 type Failure = String
 
 class Value a where
-  parse :: String -> Either Failure a
+  convert :: String -> Either Failure a
 
 newtype StringVal = StringVal String
+
+unwrap (StringVal s) = s
 
 instance Show StringVal where
   show (StringVal s) = show s
@@ -45,7 +52,7 @@ instance IsString StringVal where
   fromString = StringVal
 
 instance Value StringVal where
-  parse = Right . StringVal . unescape
+  convert = Right . StringVal . unescape
 
 unescape :: String -> String
 unescape ('\\':'s':xs)  = ' ':(unescape xs)
@@ -58,18 +65,18 @@ unescape (x:xs)         = x:(unescape xs)
 unescape []             = []
 
 instance Value Bool where
-  parse "true"  = Right True
-  parse "false" = Right False
-  parse x       = Left $ "Invalid boolean value: " ++ x
+  convert "true"  = Right True
+  convert "false" = Right False
+  convert x       = Left $ "Invalid boolean value: " ++ x
 
 instance Value Float where
-  parse s = case reads s of
+  convert s = case reads s of
     [(n, "")] -> Right n
     [(_, extra)] -> Left $ "Extra stuff after number"
     [] -> Left $ "No number to parse"
-    
+
 instance Value a => Value [a] where
-  parse s = mapM parse $ split ';' s
+  convert s = mapM convert $ split ';' s
 
 split :: Char -> String -> [String]
 split c s = unfoldr (split1' c) s
@@ -112,7 +119,7 @@ keys (Just (Locale lang Nothing (Just modifier))) =
   [Just $ Locale lang Nothing (Just modifier),
    Just $ Locale lang Nothing Nothing,
    Nothing]
-keys (Just (Locale lang Nothing Nothing)) = 
+keys (Just (Locale lang Nothing Nothing)) =
   [Just $ Locale lang Nothing Nothing,
    Nothing]
 keys Nothing = [Nothing]
@@ -125,6 +132,72 @@ getValue (Key name locale) group = do
 get :: Value a => String -> Key -> DesktopEntry -> Either Failure (Maybe a)
 get g k e = case (getGroup g e) >>= (getValue k) of
               Nothing -> Right Nothing
-              Just s -> case parse s of
+              Just s -> case convert s of
                           Left f -> Left f
                           Right v -> Right $ Just v
+
+--
+-- Parsing
+--
+
+fromContents = parse file ""
+
+fromFile = parseFromFile file
+
+blankLine = manyTill inlineSpace newline
+comment = char '#' >> manyTill anyChar (try newline)
+blankLines = skipMany (blankLine <|> comment)
+
+inlineSpace = oneOf " \t"
+
+-- group names may contain all ASCII characters except for '[' and ']' and control characters
+groupNameChars = '\\' : [' '..'Z'] ++ ['^'..'~']
+
+-- only the characters A-Za-z0-9- may be used in key names
+keyNameChars = '-' : ['A'..'Z'] ++ ['a'..'z'] ++ ['0'..'9']
+
+file :: Parser DesktopEntry
+file = do
+  blankLines
+  groups <- many aGroup
+  eof
+  return $ DesktopEntry $ M.fromList groups
+
+aGroup :: Parser (String, Group)
+aGroup = do
+  name <- (between (char '[') (char ']') groupName)
+  blankLines
+  entries <- many (entry)
+  return (name, reduceEntries entries)
+
+reduceEntries :: [(String, Maybe Locale, String)] -> Group
+reduceEntries [] = M.empty
+reduceEntries ((name, locale, value):es) =
+  M.alter (Just . M.insert locale value . fromMaybe M.empty) name (reduceEntries es)
+
+
+groupName = many1 (oneOf groupNameChars)
+
+entry :: Parser (String, Maybe Locale, String)
+entry = do
+  name <- many1 (oneOf keyNameChars)
+  locale <- option Nothing localeString
+  many inlineSpace
+  char '='
+  many inlineSpace
+  value <- manyTill anyChar newline
+  return (name, locale, value)
+
+perhaps :: Parser a -> Parser (Maybe a)
+perhaps p = (fmap Just p) <|> (return Nothing)
+
+localeString :: Parser (Maybe Locale)
+localeString = do
+  char '['
+  lang <- many1 letter
+  country <- perhaps $ char '_' >> (many1 letter)
+  modifier <- perhaps $ char '@' >> (many1 letter)
+  char ']'
+  return $ Just $ Locale lang country modifier
+
+
